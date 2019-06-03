ce77a61279f243ae5a18f6f7fac5b3b34ca1ca1c
abko <abko@me.com>
2019-06-03 19:31:04 +0200

    Attempting to fix comparison to find duplicate inputs between Features and Inputs

--- index.js
+++ index.js
@@ -147,15 +147,6 @@ function setupFromService(service) {
           InputSourceType: [0]
         });
         id++;
-        inputs.push({
-          ConfiguredName: 'SERVER',
-          ConfiguredTitle: 'SERVER',
-          Identifier: id,
-          InputDeviceType: [0],
-          InputSourceType: [0]
-        });
-        id++;
-
 
         // iterate through the feature list of the amp to add more inputs
         var zonesXML = sysConfig.YAMAHA_AV.System[0].Config[0].Feature_Existence[0];
