fa67df80af49083e329dadfa2290681dfcfb81e7
Tor André Roland <tor.andre.roland@gmail.com>
2020-04-09 15:58:02 +0200

    Added remote control for AVR

--- index.js
+++ index.js
@@ -467,27 +467,34 @@ YamahaZone.prototype = {
       .getCharacteristic(Characteristic.RemoteKey)
       .on('set', function(newValue, callback) {
         debug("setRemoteKey: ", that.zone, newValue);
-        var option = util.mapKeyToControl(newValue);
-        if (option) {
-          debug("command", that.zone, newValue, option, this.pausePlay);
-          yamaha.getBasicInfo(that.zone).then(function(basicInfo) {
-            if (basicInfo.getCurrentInput() === 'AirPlay' || basicInfo.getCurrentInput() === 'Spotify') {
-              var input = basicInfo.getCurrentInput();
-              yamaha.SendXMLToReceiver(
-                '<YAMAHA_AV cmd="PUT"><' + input + '><Play_Control><Playback>' + option + '</Playback></Play_Control></' + input + '></YAMAHA_AV>'
-              );
-            } else { // For non Spotify or Airplay sources perform Mute
-              if (newValue === Characteristic.RemoteKey.PLAY_PAUSE) {
-                if (basicInfo.isMuted(that.zone)) {
-                  debug("Mute Off: ", that.zone);
-                  yamaha.muteOff(that.zone);
-                } else {
-                  debug("Mute On : ", that.zone);
-                  yamaha.muteOn(that.zone);
-                }
-              } // end Mute functionality for non Spotiry or Airplay sources
-            }
-          });
+        switch (newValue) {
+          case Characteristic.RemoteKey.ARROW_UP:
+            yamaha.remoteCursor("Up");
+            break;
+          case Characteristic.RemoteKey.ARROW_DOWN:
+            yamaha.remoteCursor("Down");
+            break;
+          case Characteristic.RemoteKey.ARROW_RIGHT:
+            yamaha.remoteCursor("Right");
+            break;
+          case Characteristic.RemoteKey.ARROW_LEFT:
+            yamaha.remoteCursor("Left");
+            break;
+          case Characteristic.RemoteKey.SELECT:
+            yamaha.remoteCursor("Sel");
+            break;
+          case Characteristic.RemoteKey.BACK:
+            yamaha.remoteCursor("Return");
+            break;
+          case Characteristic.RemoteKey.INFORMATION:
+            yamaha.remoteMenu("On Screen");
+            break;
+          case Characteristic.RemoteKey.PLAY_PAUSE:
+            yamaha.getBasicInfo(that.zone).then(function(basicInfo) {
+              basicInfo.isMuted(that.zone) ? yamaha.muteOff(that.zone) : yamaha.muteOn(that.zone);
+            });
+            break;
+          default:
         }
         callback(null);
       });
